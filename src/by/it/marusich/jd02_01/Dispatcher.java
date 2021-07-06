0d21d9b18b1c23f874af466ee4ba5371b2560e98
marusich <marusitchdmitry@yandex.by>
2021-07-06 23:20:17 +0300

    1

---
+++ src/by/it/marusich/jd02_01/Dispatcher.java
@@ -0,0 +1,5 @@
+package by.it.marusich.jd02_01;
+
+public class Dispatcher {
+    static  int  currentCountShoppers=0;
+}
---
+++ src/by/it/marusich/jd02_03/QueueShoppers.java
@@ -0,0 +1,4 @@
+package by.it.marusich.jd02_03;
+
+public class QueueShoppers {
+}

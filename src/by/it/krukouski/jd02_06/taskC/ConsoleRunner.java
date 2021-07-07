fb9801b807d4bbb9a836e8ada1be811462565ab4
VadzimKrukouski <vadzimkrukouski@gmail.com>
2021-07-07 15:27:36 +0300

    JD 02_06 TaskC builder part1

--- src/by/it/krukouski/jd02_06/taskC/ConsoleRunner.java
+++ src/by/it/krukouski/jd02_06/taskC/ConsoleRunner.java
@@ -6,29 +6,31 @@ import java.util.Scanner;
 public class ConsoleRunner {
     static final String KEY_WORD = "end";
     public static ResMan manager = ResMan.INSTANCE;
+    public static Scanner scanner = new Scanner(System.in);
 
 
     public static void main(String[] args) throws CalcException {
         Printer printer = new Printer();
         Parser parser = new Parser();
         Time time = new Time();
-        Scanner scanner = new Scanner(System.in);
         Logger.getLogger().log("start program" + " " + time.getTime());
-        System.out.println("Choose language: be en ru");
-        String language = scanner.nextLine();
-        switch (language) {
-            case "ru":
-                manager.set(new Locale("ru", "RU"));
-                break;
-            case "en":
-                manager.set(new Locale("en", "US"));
-                break;
-            case "be":
-                manager.set(new Locale("be", "BY"));
-                break;
-        }
+        chooseLanguage(scanner);
         System.out.println(manager.get(Errors.keyWord));
+        calc(printer, parser, scanner);
+        createReport();
+        Logger.getLogger().log("finish program" + " " + time.getTime());
+    }
 
+    private static void createReport() {
+        WriterReport writerReport = new WriterReport();
+        ReportBuilder reportBuilder = new MainReport();
+        writerReport.setReportBuilder(reportBuilder);
+        writerReport.constructReport();
+        Report report =writerReport.getReportBuilder();
+        System.out.println(report);
+    }
+
+    private static void calc(Printer printer, Parser parser, Scanner scanner) {
         for (; ; ) {
             String expression = scanner.nextLine();
             if (!expression.equals(KEY_WORD)) {
@@ -44,6 +46,21 @@ public class ConsoleRunner {
                 break;
             }
         }
-        Logger.getLogger().log("finish program" + " " + time.getTime());
+    }
+
+    private static void chooseLanguage(Scanner scanner) {
+        System.out.println("Choose language: be en ru");
+        String language = scanner.nextLine();
+        switch (language) {
+            case "ru":
+                manager.set(new Locale("ru", "RU"));
+                break;
+            case "en":
+                manager.set(new Locale("en", "US"));
+                break;
+            case "be":
+                manager.set(new Locale("be", "BY"));
+                break;
+        }
     }
 }
